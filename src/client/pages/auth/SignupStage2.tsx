3240e63062f06a81b465e405777a611148ad2e75
Sergey Kalugin <kalugin.serej@gmail.com>
2023-05-02 02:44:43 +0300

    Redirect to SignUp stage 2

---
+++ src/client/pages/auth/SignupStage2.tsx
@@ -0,0 +1,19 @@
+export function SignupStage2() {
+  return (
+    <div className="h-full w-full bg-white">
+      <div className="flex min-h-[75vh] min-w-full items-center justify-center">
+        <div className="h-full w-full max-w-sm bg-white p-5">
+          <div>
+            <h1>Signup: stage 2</h1>
+            <p>Now, let's fill in additional info ={"}"}</p>
+            <br />
+            <span className="text-sm font-medium text-gray-900">
+              I already have an account (I don't).
+            </span>
+            <br />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
