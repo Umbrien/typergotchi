803cc440c95aafeea4c95481e9feb8cad7b4affe
Sergey Kalugin <kalugin.serej@gmail.com>
2023-05-02 22:05:21 +0300

    add vscode config to .prettierrc

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,23 @@
 module.exports = {
+  "arrowParens": "always",
+  "bracketSpacing": true,
+  "endOfLine": "lf",
+  "htmlWhitespaceSensitivity": "css",
+  "insertPragma": false,
+  "singleAttributePerLine": false,
+  "bracketSameLine": false,
+  "jsxBracketSameLine": false,
+  "jsxSingleQuote": false,
+  "printWidth": 80,
+  "proseWrap": "preserve",
+  "quoteProps": "as-needed",
+  "requirePragma": false,
+  "semi": true,
+  "singleQuote": false,
+  "tabWidth": 2,
+  "trailingComma": "es5",
+  "useTabs": false,
+  "vueIndentScriptAndStyle": false,
   // ... rest of your config
   plugins: ["prettier-plugin-tailwindcss"],
 };
