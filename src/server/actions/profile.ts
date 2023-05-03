ad5bdb7fa4d91a8b6d4f913dcd6c5d8e4ac9b802
kg04ls <layup28@gmail.com>
2023-05-03 15:42:00 +0200

    Add "sedDaBoiArmor" action

--- src/server/actions/profile.ts
+++ src/server/actions/profile.ts
@@ -1,5 +1,5 @@
 import HttpError from '@wasp/core/HttpError.js';
-import { Context } from "../_types";
+import { Context, inventoryContext } from "../_types";
   
 export async function changeNickname(
   { nickname }: { nickname: string },
@@ -8,10 +8,10 @@ export async function changeNickname(
   if (!context.user) {
     throw new HttpError(401);
   }
-  const nickname_used = await context.entities.User.findUnique({
+  const nicknameUsed = await context.entities.User.findUnique({
     where: { nickname }
   });
-  if (nickname_used){
+  if (nicknameUsed){
     throw new HttpError(409);
   }
   return context.entities.User.update({
@@ -22,8 +22,8 @@ export async function changeNickname(
   })
 }
 
-export async function changeDaBoiSkin(
-  { daBoiId }: { daBoiId: number },
+export async function setDaBoiSkin(
+  { daBoiSkinId }: { daBoiSkinId: number },
   context: Context
 ){
   if (!context.user) {
@@ -32,7 +32,31 @@ export async function changeDaBoiSkin(
   return context.entities.User.update({
     where: { id: context.user.id },
     data: {
-      daBoiSelectedSkinId: daBoiId
+      daBoiSelectedSkinId: daBoiSkinId
     }
   })
+}
+
+export async function setDaBoiArmor(
+  { daBoiArmorId }: { daBoiArmorId: number },
+  context: inventoryContext
+){
+  if (!context.user) {
+    throw new HttpError(401);
+  }
+  const userHaveSkin = await context.entities.Inventory.findUnique({
+    where: {
+      userId: context.user.id,
+      armorId: daBoiArmorId
+    }
+  });
+  if (!userHaveSkin){
+    throw new HttpError(409);
+  }
+  return context.entities.User.update({
+    where: { id: context.user.id },
+    data: {
+      daBoiSelectedArmorId: daBoiArmorId
+    }
+  })  
 }
