94fe0dd91ebe9f1c4f54b9b34334a7b659a0367a
Sergey Kalugin <kalugin.serej@gmail.com>
2023-05-04 03:50:49 +0300

    registration stage 2

--- src/client/pages/auth/SignupStage2.tsx
+++ src/client/pages/auth/SignupStage2.tsx
@@ -1,14 +1,52 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import { useQuery } from "@wasp/queries";
 import fetchDaBoiSkins from "@wasp/queries/fetchDaBoiSkins";
+import registerStep2 from "@wasp/actions/registerStep2";
 
 export function SignupStage2() {
+  const history = useHistory();
+
   const { data: daBoiSkins } = useQuery(fetchDaBoiSkins);
+
+  const [nickname, setNickname] = useState("");
+  const [nickNameError, setNicknameError] = useState(false);
+  const [bio, setBio] = useState("");
+  const [selectedSkinId, setSelectedSkinId] = useState<number | null>(null);
+  const [selectedSkinIdError, setSelectedSkinIdError] = useState(false);
+
+  function handleSkinClick(skinId: number) {
+    setSelectedSkinId((id) => (id === skinId ? null : skinId));
+  }
+
+  async function handleFinishSignupClick() {
+    const nickNameIsValid = nickname.length > 0;
+    const selectedSkinIdIsValid = selectedSkinId !== null;
+
+    setNicknameError(!nickNameIsValid);
+    setSelectedSkinIdError(!selectedSkinIdIsValid);
+
+    if (nickNameIsValid && selectedSkinIdIsValid) {
+      console.log(nickname, bio, selectedSkinId);
+      try {
+        const user = await registerStep2({
+          nickname,
+          bio,
+          daBoiSelectedSkinId: selectedSkinId,
+        });
+        console.log(user);
+      } catch (err) {
+        console.log(err);
+      }
+      history.push("/");
+    }
+  }
 
   return (
     <div className="h-full w-full bg-white">
       <div className="flex min-h-[75vh] min-w-full items-center justify-center">
         <div className="h-full w-full max-w-sm bg-white p-5">
-          <div>
+          <div className="flex flex-col">
             <h1>Signup: stage 2</h1>
             <p>
               Now, let's fill in additional info{" "}
@@ -17,26 +55,28 @@ export function SignupStage2() {
               </span>
             </p>
 
-            <label className="block">
+            <label className="my-1 block">
               <span className="text-gray-700">Nickname</span>
               <input
                 type="text"
-                className="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-transparent
-                    bg-gray-100
-                    focus:border-yellow-500 focus:bg-white focus:ring-0
-                  "
+                value={nickname}
+                onInput={(e) => setNickname(e.currentTarget.value)}
+                className="mt-1 block w-full rounded-md border-transparent bg-gray-100
+                    focus:border-yellow-500 focus:bg-white focus:ring-0"
                 placeholder=""
               />
+              {nickNameError && (
+                <span className="text-sm text-red-700">
+                  Come on, fill in cool nickname
+                </span>
+              )}
             </label>
 
-            <label className="block">
+            <label className=" my-1 block">
               <span className="text-gray-700">Bio</span>
               <textarea
+                value={bio}
+                onInput={(e) => setBio(e.currentTarget.value)}
                 className="
                     mt-1
                     block
@@ -50,18 +90,39 @@ export function SignupStage2() {
               ></textarea>
             </label>
 
-            {daBoiSkins &&
-              // @ts-ignore
-              daBoiSkins.map(({ id, img, name }) => (
-                <div className="flex items-center" key={`daboi-skin-${id}`}>
-                  <img src={img} alt={name} className="h-10 w-10" />
-                  <span className="ml-2">{name}</span>
+            {daBoiSkins && (
+              <div className="my-4 flex flex-col">
+                <span className="self-center text-gray-700">
+                  Choose your da boi
+                </span>
+                {selectedSkinIdError && (
+                  <span className="text-sm text-red-700">
+                    Choose your boi, really
+                  </span>
+                )}
+                <div className="my-3 grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3">
+                  {daBoiSkins.map(({ id, img, name }) => (
+                    <div
+                      className={`flex flex-col items-center rounded-md border border-yellow-400 p-3 ${
+                        selectedSkinId === id ? "bg-yellow-50" : ""
+                      } transition hover:rounded-xl hover:bg-yellow-50 active:bg-yellow-100`}
+                      key={`daboi-skin-${id}`}
+                      onClick={() => handleSkinClick(id)}
+                    >
+                      <img src={img} alt={name} />
+                      <span className="ml-2">{name}</span>
+                    </div>
+                  ))}
                 </div>
-              ))}
+              </div>
+            )}
 
-            <span className="text-sm font-medium text-gray-900">
-              I already have an account (I don't).
-            </span>
+            <button
+              className="self-end rounded-md bg-yellow-50 px-4 py-2 transition hover:rounded-lg hover:bg-yellow-100 active:bg-yellow-200"
+              onClick={handleFinishSignupClick}
+            >
+              Finish signup
+            </button>
             <br />
           </div>
         </div>
--- src/server/actions/profile.ts
+++ src/server/actions/profile.ts
@@ -1,38 +1,71 @@
-import HttpError from '@wasp/core/HttpError.js';
+import HttpError from "@wasp/core/HttpError.js";
 import { Context } from "../_types";
-  
+
+export async function registerStep2(
+  {
+    nickname,
+    bio,
+    daBoiSelectedSkinId,
+  }: {
+    nickname: string;
+    bio?: string;
+    daBoiSelectedSkinId: number;
+  },
+  context: Context
+) {
+  if (!context.user) {
+    throw new HttpError(401);
+  }
+
+  const nicknameIsUsed = await context.entities.User.findUnique({
+    where: { nickname },
+  });
+  if (nicknameIsUsed) {
+    throw new HttpError(409);
+  }
+
+  return context.entities.User.update({
+    where: { id: context.user.id },
+    data: {
+      nickname,
+      description: bio,
+      daBoiSelectedSkinId,
+    },
+  });
+}
+
 export async function changeNickname(
   { nickname }: { nickname: string },
   context: Context
-){
+) {
   if (!context.user) {
     throw new HttpError(401);
   }
   const nickname_used = await context.entities.User.findUnique({
-    where: { nickname }
+    where: { nickname },
   });
-  if (nickname_used){
+  if (nickname_used) {
     throw new HttpError(409);
   }
   return context.entities.User.update({
     where: { id: context.user.id },
     data: {
-      nickname
-    }
-  })
+      nickname,
+    },
+  });
 }
 
 export async function changeDaBoiSkin(
   { daBoiId }: { daBoiId: number },
   context: Context
-){
+) {
   if (!context.user) {
     throw new HttpError(401);
   }
   return context.entities.User.update({
     where: { id: context.user.id },
     data: {
-      daBoiSelectedSkinId: daBoiId
-    }
-  })
+      daBoiSelectedSkinId: daBoiId,
+    },
+  });
 }
