2270037167baa26ca87e9558632d7d69351c96b8
Sergey Kalugin <kalugin.serej@gmail.com>
2023-05-06 21:13:12 +0300

    display words by blocks of 50

--- src/client/components/typer/SoloModeSession.tsx
+++ src/client/components/typer/SoloModeSession.tsx
@@ -1,18 +1,32 @@
 import { Word } from "./Word";
 
-export function SoloModeSession({ text }: { text?: string[] }) {
+const wordsPerBlock = 50;
+
+export function SoloModeSession({
+  text,
+  currentWordIndex,
+}: {
+  text?: string[];
+  currentWordIndex: number;
+}) {
+  const blockStart =
+    Math.floor(currentWordIndex / wordsPerBlock) * wordsPerBlock;
+  const blockEnd = blockStart + wordsPerBlock;
+
   return (
     <div className="flex-start flex h-48 flex-wrap items-start gap-2 rounded-xl bg-yellow-200 p-3">
       {text ? (
-        text.map((word, i) => (
-          <Word
-            key={i}
-            word={word}
-            index={i}
-            currentWordIndex={0}
-            isCurrentWord={i === 0}
-          />
-        ))
+        text
+          .slice(blockStart, blockEnd)
+          .map((word, i) => (
+            <Word
+              key={i}
+              word={word}
+              index={i}
+              currentWordIndex={currentWordIndex}
+              isCurrentWord={blockStart + i === currentWordIndex}
+            />
+          ))
       ) : (
         <span>Loading...</span>
       )}
