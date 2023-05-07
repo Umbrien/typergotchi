fbb6f9664fd9bf1231a7e852e0c6ccf7fcf15912
kg04ls <layup28@gmail.com>
2023-05-07 18:50:43 +0200

    add dbSeeding

---
+++ src/server/dbSeeds.ts
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+export async function seedDaBoiSkins(prismaClient: PrismaClient) {
+  /*console.log("sdkj");
+  const daBoiFolder = "preboiz/";
+  await prismaClient.daBoiSkin.createMany({
+    data: [
+      {
+        name: "Boi",
+        imgHappy: daBoiFolder + "default_happ.png",
+        imgSad: daBoiFolder + "default_sad.png",
+      },
+      {
+        name: "SpaceBoi",
+        imgHappy: daBoiFolder + "space_happ.png",
+        imgSad: daBoiFolder + "space_sad.png",
+      },
+      {
+        name: "SpiceyBoi",
+        imgHappy: daBoiFolder + "green_happ.png",
+        imgSad: daBoiFolder + "green_sad.png",
+      },
+    ],
+  });*/
+}
+
+export async function seedDaBoiArmors(prismaClient: PrismaClient) {
+  const armorFolder = "armor/";
+  await prismaClient.daBoiArmor.createMany({
+    data: [
+      {
+        name: "wasp",
+        price: 100,
+        img: armorFolder + "logo.png",
+      },
+      {
+        name: "Christmas",
+        price: 10,
+        img: armorFolder + "MC.png",
+      },
+      {
+        name: "Top hat",
+        price: 8,
+        img: armorFolder + "gentleboi.png",
+      },
+    ],
+  });
+}
